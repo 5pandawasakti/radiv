1afc526295c3a6611b43f3cfc571c7bda509ece9
5pandawasakti <36889274+5pandawasakti@users.noreply.github.com>
2021-03-10 01:24:56 +0700

    CFX NBM

---
+++ nbm/cfx_pool.sh
@@ -0,0 +1,1 @@
+./cfx -a octopus -o stratum+tcp://conflux.herominers.com:10221 -u 0x14ad0cC5e03Be67AC56bA8abb0EeDDE4Ee739c79.X1
---
+++ nbm/cfx_solo.sh
@@ -0,0 +1,1 @@
+./cfx -a octopus -o stratum+tcp://conflux.herominers.com:10221 -u solo:0x14ad0cC5e03Be67AC56bA8abb0EeDDE4Ee739c79.X1Solo
